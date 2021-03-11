5b2516fc4e6feb990980f9a3141be46c5e80c75b
lakkvak <estrovsky111@gmail.com>
2021-03-11 20:56:37 +0300

    Update

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,12 +1,99 @@
-unsigned int faStr1(const char *str)
+#include "fun.h"
+#include <iostream>
+unsigned int faStr1(const char* str)
 {
-    return 0;
+	bool isWord = false;	
+	int i = 0, count = 0;
+
+	while (str[i] != '\0')
+	{
+		if ((str[i] > 57 || str[i] < 48) && str[i]!=' ')
+		{
+			isWord = true;
+			
+			while (str[i] != ' ' && str[i]!='\0')
+			{
+				i++;
+				if (str[i] > 47 && str[i] < 58 && isWord )
+					isWord = false;
+			}
+			
+			if (isWord) {
+				count++; 
+				isWord = false;
+			}
+		}
+		else		
+			while (str[i] != ' ' && str[i] != '\0')
+				i++;			
+		
+		while (str[i] == ' ')
+			i++;		
+	}
+	return count;
 }
-unsigned int faStr2(const char *str)
+unsigned int faStr2(const char* str)
 {
-    return 0;
+	bool isWord = false;
+	int i = 0, count = 0;
+
+	while (str[i] != '\0')
+	{
+		if ((str[i] > 64 && str[i] < 91) && str[i] != ' ')
+		{
+			isWord = true;
+			i++;
+			while (str[i] != ' ' && str[i] != '\0')
+			{
+				
+				if ((str[i] < 97 || str[i]>122 ) && isWord)
+					isWord = false;
+				i++;
+			}
+
+			if (isWord) {
+				count++;
+				isWord = false;
+			}
+		}
+		else
+			while (str[i] != ' ' && str[i] != '\0')
+				i++;
+
+		while (str[i] == ' ')
+			i++;
+	}
+	return count;
 }
-unsigned int faStr3(const char *str)
+unsigned int faStr3(const char* str)
 {
-    return 0;
+	int i = 0, countWord = 0, result = 0;
+	float countLett=0;	
+
+	while (str[i] != '\0')
+	{
+		if (str[i]!=' ')
+		{
+						
+			while (str[i] != ' ' && str[i] != '\0')
+			{
+				countLett++;
+				i++;
+			}
+			countWord++;		
+		}
+		else
+		{
+			while (str[i]== ' ' )
+				i++;				
+		}
+
+		
+	}
+	countLett = countLett / static_cast<float>(countWord);
+	result = countLett;
+	
+	if (countLett - static_cast<float>(result) >= 0.5)
+		result++;
+	return result;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
-#include "fun.h"
+﻿// Lab1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
 
+#include <iostream>
+#include "fun.h"
 int main()
 {
-    return 0;
+    
+    std::cout << faStr1("fgb f3f 3  ") << "\n";
+    std::cout << faStr2("DFdfdf Ld2ffcv  Okc gkgf") << "\n";
+    std::cout << faStr3("DFdfdf Ld2ff rr ") << "\n";
 }
+
+
